d762f633cc24c54107e28aa0d2d1579a6eee2ebc
imashnyov <imashnyov59901@gmail.com>
2021-05-29 20:26:06 +0300

    work IaS

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,7 +1,9 @@
 provider "aws" {
   region = "us-east-2"
 }
-
+/*
+#-------------------------EC2--------------------------
+# latest instances
 data "aws_ami" "latest_ubuntu" {
   owners      = ["099720109477"]
   most_recent = true
@@ -21,7 +23,7 @@ data "aws_ami" "latest_amazon_linux" {
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
-
+# create instances
 resource "aws_instance" "project" {
   ami                    = data.aws_ami.latest_ubuntu.id
   instance_type          = "t2.micro"
@@ -44,7 +46,7 @@ resource "aws_instance" "jenkins" {
     Name = "Jenkins"
   }
 }
-
+# create policy
 resource "aws_security_group" "project_security" {
   name = "web-server-security"
 
@@ -132,7 +134,7 @@ resource "aws_security_group" "jenkins_security" {
     Name = "ssh_8080"
   }
 }
-
+# console output
 output "latest_ubuntu_ami_id" {
   value = data.aws_ami.latest_ubuntu.id
 }
@@ -155,7 +157,7 @@ output "jenkins_server_jenkins_public_ip" {
   value = aws_instance.jenkins.public_ip
 }
 
-
+# generate hosts file for ansible
 resource "null_resource" "command1" {
   provisioner "local-exec" {
     command = "echo Ubuntu: ${aws_instance.project.public_ip} Amazon_linux: ${aws_instance.jenkins.public_ip} > project_public_ip.txt"
@@ -169,3 +171,69 @@ resource "null_resource" "command2" {
   }
   depends_on = [null_resource.command1]
 }
+*/
+/*
+#--------------------------S3--------------------------
+
+resource "aws_s3_bucket" "front_of_project" {
+  bucket = "imashnov-front-of-project"
+  acl    = "public-read"
+
+  tags = {
+    Name        = "Front"
+    Environment = "PROD"
+  }
+  cors_rule {
+    allowed_headers = ["*"]
+    allowed_methods = ["PUT", "POST"]
+    allowed_origins = ["https://imashnov-front-of-project"]
+    expose_headers  = ["ETag"]
+    max_age_seconds = 3000
+  }
+  #Using versioning
+  /*
+  versioning {
+    enabled = true
+  }
+}
+*/
+/*
+}
+
+output "arn_s3" {
+  value = aws_s3_bucket.front_of_project.arn
+}
+
+output "s3_full_name" {
+  value = aws_s3_bucket.front_of_project.bucket
+}
+
+output "s3_short_name" {
+  value = aws_s3_bucket.front_of_project.bucket_prefix
+}
+*/
+#--------------------------RDS--------------------------
+resource "aws_db_instance" "beer_database" {
+  allocated_storage    = 5
+  max_allocated_storage = 7
+  engine               = "postgres"
+  engine_version       = "12.5"
+  instance_class       = "db.t2.micro"
+  name                 = "beerdb1"
+  username             = "imashnyov"
+  password             = "beeradmin"
+  skip_final_snapshot  = true
+#  parameter_group_name = "rds_sg"
+}
+/*
+resource "aws_db_security_group" "rds_sg" {
+  name = "rds_sg"
+
+  ingress {
+    from_port        = 1453
+    to_port          = 1453
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+*/
