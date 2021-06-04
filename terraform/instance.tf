d2b13fef26531e614b155b6cc092cac4232c829c
imashnyov <imashnyov59901@gmail.com>
2021-06-04 22:56:44 +0300

    added dockerfile for jenkins

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -140,6 +140,13 @@ resource "aws_security_group" "jenkins_security" {
   }
 
   ingress {
+    from_port        = 8096
+    to_port          = 8096
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
